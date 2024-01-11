b692f7d1cea5cb3ca94801dfde1d3e1b4506eb9e
Yanna <yanna@Yannas-MacBook-Pro.local>
2024-01-11 15:21:32 -0800

    step 10

--- packages/react-frontend/src/Table.js
+++ packages/react-frontend/src/Table.js
@@ -6,6 +6,7 @@ function TableHeader() {
         <tr>
           <th>Name</th>
           <th>Job</th>
+          <th>Delete</th>
         </tr>
       </thead>
     );
@@ -19,6 +20,11 @@ function TableHeader() {
         <tr key={index}>
           <td>{row.name}</td>
           <td>{row.job}</td>
+          <td>
+            <button onClick={() => props.removeCharacter(index)}>
+              Delete
+            </button>
+          </td>
         </tr>
       );
      }
@@ -31,14 +37,17 @@ function TableHeader() {
   }
   
 
-function Table(props) {
+  function Table(props) {
     return (
-        <table>
-          <TableHeader />
-          <TableBody characterData={props.characterData} />
-        </table>
-      );
-}
+      <table>
+        <TableHeader />
+        <TableBody
+          characterData={props.characterData}
+          removeCharacter={props.removeCharacter}
+        />
+      </table>
+    );
+  }
 
 export default Table;
 
