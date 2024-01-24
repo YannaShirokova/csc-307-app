23e45ccdb09c1dda31f1bae43a9831a9b240ad7a
Yanna <shirokovayanna@gmail.com>
2024-01-24 14:09:51 -0800

    CORS

--- packages/react-frontend/src/MyApp.js
+++ packages/react-frontend/src/MyApp.js
@@ -1,12 +1,8 @@
-import React, { useState } from "react";
+import React, {useState, useEffect} from 'react';
 import Table from "./Table";
 import Form from "./Form";
 
   function MyApp() {
-
-   
-    
-
     const [characters, setCharacters] = useState([]);
     function removeOneCharacter(index) {
       const updated = characters.filter((character, i) => {
@@ -18,9 +14,22 @@ import Form from "./Form";
     function updateList(person) {
       setCharacters([...characters, person]);
     }
-    
-    
 
+    // get table from backend by fetching
+    function fetchUsers() {
+      //makes a request url
+      const promise = fetch("http://localhost:8000/users");
+      //returns immediately with a promise
+      return promise;
+    }
+
+    useEffect(() => {
+      fetchUsers()
+        .then((res) => res.json()) // res = object, reads response, pass to next one
+        .then((json) => setCharacters(json["users_list"])) // set chars
+        .catch((error) => { console.log(error); });
+    }, [] ); //empty array means its done once when rendered
+    
     return (
       <div className="container">
         <Table
